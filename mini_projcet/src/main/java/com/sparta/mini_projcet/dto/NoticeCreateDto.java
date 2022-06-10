f2abb522d5a23746e8b1a34ed6bce03ab065788e
ParkSungGyu1 <steven0507@naver.com>
2022-06-10 17:20:14 +0900

    notice 생성 api추가

--- mini_projcet/src/main/java/com/sparta/mini_projcet/dto/NoticeCreateDto.java
+++ mini_projcet/src/main/java/com/sparta/mini_projcet/dto/NoticeCreateDto.java
@@ -3,10 +3,14 @@ package com.sparta.mini_projcet.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+
 @Data
 @AllArgsConstructor
 public class NoticeCreateDto {
+    @NotNull(message = "제목을 입력해 주세요.")
     private String title;
+    @NotNull(message = "내용을 입력해 주세요.")
     private String description;
-    private String username;
 }
