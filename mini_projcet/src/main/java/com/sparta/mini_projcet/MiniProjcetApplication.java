a3267ea20a474a0b7c0f70f091cda49b34e400a6
lky8967 <lky8967@gmail.com>
2022-06-10 14:34:24 +0900

    프로젝트 생성 후 커밋

---
+++ mini_projcet/src/main/java/com/sparta/mini_projcet/MiniProjcetApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.mini_projcet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjcetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjcetApplication.class, args);
+	}
+
+}
---
+++ mini_projcet/src/test/java/com/sparta/mini_projcet/MiniProjcetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.mini_projcet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjcetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
